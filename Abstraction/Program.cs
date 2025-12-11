1c3c81620cc193217308de084944abdc78aab1a6
Zambo Dominik <zambo.dominik@puskas.hu>
2025-12-11 16:22:39 +0100

    Abstraction and dependency injection added

---
+++ Abstraction/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Abstraction
+{
+    internal class Program
+    {
+        public interface IAlakzat
+        {
+            //string Name//{get;set;}
+            double Terulet();
+            double Kerulet();
+
+            virtual void PrintInfo()
+            {
+                Console.WriteLine("Alakzat info");
+            }
+
+        }
+        public abstract class Alakzat
+        {
+            public abstract double Terulet();
+            public abstract double Kerulet();
+
+            public string Name;
+            protected Alakzat(string name)
+            {
+                Name = name;
+            }
+        }
+        public class Kor : Alakzat,IAlakzat
+        {
+            public double Sugar { get; set; }
+            public Kor(double sugar):base("Kor")
+            {
+                Sugar = sugar;
+            }
+            public override double Terulet()
+            {
+                return Math.PI * Sugar * Sugar;
+            }
+            public override double Kerulet()
+            {
+                return 2 * Math.PI * Sugar;
+            }
+        }
+        public class Negyzet : Alakzat
+        {
+            public double Oldal { get; set; }
+            public Negyzet(double oldal):base("Negyzet")
+            {
+                Oldal = oldal;
+            }
+            public override double Terulet()
+            {
+                return Oldal * Oldal;
+            }
+            public override double Kerulet()
+            {
+                return 4 * Oldal;
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Kor kor = new Kor(5);
+            Negyzet negyzet = new Negyzet(4);
+            Alakzat[] alakzatok = new Alakzat[] { kor, negyzet };
+            foreach (Alakzat alakzat in alakzatok)
+            {
+                Console.WriteLine(alakzat.Name);
+                
+                Console.WriteLine($"Terulet: {alakzat.Terulet()}");
+                Console.WriteLine($"Kerulet: {alakzat.Kerulet()}");
+            }
+        }
+    }
+}
---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace DependencyInjection
+{
+    internal class Program
+    {
+
+        //webshop példa
+        public interface IFizetes
+        {
+            void Fizetes(int osszeg);
+        }
+
+        public class BankKartya : IFizetes
+        {
+            public void Fizetes(int osszeg) => Console.WriteLine($"Bankkártyáról levonva: {osszeg} Ft");
+        }
+
+        public class PayPal : IFizetes
+        {
+            public void Fizetes(int osszeg) => Console.WriteLine($"PayPal-ról utalva: {osszeg} Ft");
+        }
+
+        public class Kartya 
+        {
+            int osszeg;
+            public Kartya(int osszeg) 
+            {
+                this.osszeg = osszeg;
+            }
+
+            public void Fizetes(int mennyi) 
+            {
+                Console.WriteLine($"Bankkártyáról levonva: {mennyi} Ft");
+                osszeg -= mennyi;
+
+            }
+        }
+
+
+        public class WebshopRendeles
+        {
+            private readonly IFizetes _fizetesiMod;
+
+            Kartya _kartya;
+
+            public WebshopRendeles(IFizetes fizetesiMod)
+            {
+                _fizetesiMod = fizetesiMod;
+            }
+
+            public WebshopRendeles(Kartya kartya)
+            {
+                _kartya = kartya;
+            }
+
+            public void VasarlasBefejezese(int osszeg)
+            {
+                Console.WriteLine("Fizetés");
+                _fizetesiMod.Fizetes(osszeg);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            // A vevő Bankkártyát választ
+            IFizetes kartya = new BankKartya();
+           //hibás gondolatmenet
+            WebshopRendeles rendeles = new WebshopRendeles(new Kartya(20000));
+
+
+            WebshopRendeles rendeles1 = new WebshopRendeles(kartya);
+            rendeles1.VasarlasBefejezese(5000);
+
+            Console.WriteLine("---");
+
+            // "B" eset: A vevő PayPalt választ
+            IFizetes paypal = new PayPal();
+            WebshopRendeles rendeles2 = new WebshopRendeles(paypal);
+            rendeles2.VasarlasBefejezese(10000);
+        }
+    }
+}
